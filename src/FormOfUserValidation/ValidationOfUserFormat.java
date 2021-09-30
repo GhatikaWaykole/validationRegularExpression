c83de74c31f99ebdf8dc61bbde60c4ebb43c1e95
ghatikawaykole <ghatika99@gmail.com>
2021-09-30 12:49:43 +0530

    Adding the method to validation of the number compute

--- src/FormOfUserValidation/ValidationOfUserFormat.java
+++ src/FormOfUserValidation/ValidationOfUserFormat.java
@@ -7,9 +7,9 @@ public class ValidationOfUserFormat {
    public static Pattern pattern;
    public static Matcher matcher;
    String regx = "^[A-Z]{1}[a-z]{2,}\\s[A-Z]{1}[a-z]{2,}";//validation of regex word have first name beginig with capital and white space in the last name
-    String regex = "^[A-Za-z0-9.]+@[a-z.]+[a-z]$";         //validate the name of the word
-    String reegex = "^[0-9]{2}\\s[0-9]{10}$";
-    String numberregex = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}";
+    String regex = "^[A-Za-z0-9.]+@[a-z.]+[a-z]$";         //validation the name of the word
+    String reegex = "^[0-9]{2}\\s[0-9]{10}$";              //number of the words the validation
+    String numberregex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^-+=()]){1}[a-zA-Z0-9]{8,}";
     // create method and check the name validation with the regx define string
     public static void validationOfWord(String regx,String name){
         pattern = Pattern.compile(regx);
@@ -23,6 +23,7 @@ public class ValidationOfUserFormat {
             System.out.println("Name should begin with capital letter and have space in first name and last");
             }
         }
+        //create method validation to the number
         public void validateOfEmail(String name){
         validationOfWord(regex,name);
         Boolean val = matcher.matches();
